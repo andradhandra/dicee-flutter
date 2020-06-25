71f420bee404d2586945970b187016e879085281
ridza adhandra <andradhandra@gmail.com>
2020-06-25 13:19:35 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +17,43 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int num1 = 1;
+  int num2 = 1;
+  var random = new Random();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Flex(
+      direction: Axis.vertical,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Flex(
+          direction: Axis.horizontal,
+          children: <Widget>[
+            Expanded(
+                child: FlatButton(
+              child: Image.asset('images/dice$num1.png'),
+              onPressed: () => {
+                setState(() => {num1 = random.nextInt(6) + 1}),
+                print('dice $num1')
+              },
+            )),
+            Expanded(
+                child: FlatButton(
+              child: Image.asset('images/dice$num2.png'),
+              onPressed: () => {
+                setState(() => {num2 = random.nextInt(6) + 1}),
+                print('dice $num2')
+              },
+            ))
+          ],
+        ),
+      ],
+    );
   }
 }
